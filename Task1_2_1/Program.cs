5496a39532b93bdd1c3eff65abd878880ebef1d0
margobaklan <margo.baklan@gmail.com>
2022-10-03 01:43:03 +0300

    1

---
+++ Task1_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введіть число: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ви ввели число {n}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
+}
---
+++ Task1_2_2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1_2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show($"Ви ввели число {n}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
